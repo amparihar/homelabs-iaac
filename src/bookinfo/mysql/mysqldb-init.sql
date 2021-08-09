86eb179a1fd5aaf312658fbbb4f858382d282a44
EC2 Default User <ec2-user@ip-172-31-24-141.ap-south-1.compute.internal>
2021-08-09 08:06:48 +0000

    updated mysql Dockerfile

--- src/bookinfo/mysql/mysqldb-init.sql
+++ src/bookinfo/mysql/mysqldb-init.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE bookinfo;
-USE bookinfo;
+CREATE DATABASE test;
+USE test;
 
 CREATE TABLE `ratings` (
   `ReviewID` INT NOT NULL,
   `Rating` INT,
   PRIMARY KEY (`ReviewID`)
 );
+
+INSERT INTO ratings (ReviewID, Rating) VALUES (1, 5);
+INSERT INTO ratings (ReviewID, Rating) VALUES (2, 4);
