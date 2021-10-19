94a092e93ecfad1175612c4bf347380ec2c39b4f
EC2 Default User <ec2-user@ip-172-31-13-124.us-east-2.compute.internal>
2021-10-19 06:44:34 +0000

    meshed updates

--- terraform-infra/eks-addons/variables.tf
+++ terraform-infra/eks-addons/variables.tf
@@ -40,18 +40,18 @@ variable "app_namespaces" {
 
 variable app_xray_tracing_enabled {
   type = bool
-  default = false
+  default = true
   description = "Variable indicating whether x-ray tracing is enabled"
 }
 
 variable "app_appmesh_controller_enabled" {
   type        = bool
-  default     = false
+  default     = true
   description = "Variable indicating whether appmesh deployment is enabled"
 }
 
 variable "app_kubernetes_external_secrets_enabled" {
   type        = bool
-  default     = true
+  default     = false
   description = "Variable indicating whether k8s external secrets deployment is enabled"
 }
