8510c84709988e4da500639b43273be94dcee567
amit <amit.parihar@gmail.com>
2021-12-30 22:26:46 +0530

    Added new variable for NAT Count

--- terraform-infra/eks/vpc/vars.tf
+++ terraform-infra/eks/vpc/vars.tf
@@ -14,3 +14,8 @@ variable "cluster_name" {}
 variable "multi_az_deployment" {
   type = bool
 }
+variable "nat_gateways" {
+  type    = number
+  default = 1
+  description = "Number of NAT Gateways to be provisioned. This number cannot exceed the total number of Public Subnets in all AZs"
+}
