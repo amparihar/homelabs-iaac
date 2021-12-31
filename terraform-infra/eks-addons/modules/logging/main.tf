1dc6773016f3c2f1a0143b0aaf86d4df533fee7f
EC2 Default User <ec2-user@ip-172-31-25-117.us-east-2.compute.internal>
2021-12-31 08:23:23 +0000

    updated meshed-todos-api manifests

--- terraform-infra/eks-addons/modules/logging/main.tf
+++ terraform-infra/eks-addons/modules/logging/main.tf
@@ -3,7 +3,7 @@ resource "kubernetes_namespace" "aws_observability" {
   metadata {
     name = "aws-observability"
     labels = {
-      aws-observability = "enabled"
+      "aws-observability" = "enabled"
     }
   }
 }
@@ -14,6 +14,6 @@ resource "kubernetes_config_map" "aws_logging" {
     namespace = kubernetes_namespace.aws_observability.id
   }
   data = {
-    "output.conf" = "[OUTPUT]\n    Name cloudwatch_logs\n    Match   *\n    region ${var.region_id}\n    log_group_name /aws/eks/${var.cluster_name}/fluent-bit-cloudwatch\n    log_stream_prefix from-fluent-bit-\n    auto_create_group On\n"
+    "output.conf" = "[OUTPUT]\n    Name cloudwatch_logs\n    Match   *\n    region ${var.region_id}\n    log_group_name /aws/eks/${var.cluster_name}/fluent-bit-cloudwatch\n    log_stream_prefix from-fluent-bit-\n    auto_create_group true\n"
   }
 }
