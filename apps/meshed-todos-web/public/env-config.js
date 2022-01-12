a3043c4d3602ed3726d25f846fc3f747a18fb52c
amit <amit.parihar@gmail.com>
2022-01-12 11:28:42 +0530

    updated base url's to that of cloud front

--- apps/meshed-todos-web/public/env-config.js
+++ apps/meshed-todos-web/public/env-config.js
@@ -1,5 +1,5 @@
 window.API_BASE_URL = {
-  USER: "https://7be2yaj3m1.execute-api.us-east-2.amazonaws.com/prod/api/user/router/api",
-  GROUP: "https://7be2yaj3m1.execute-api.us-east-2.amazonaws.com/prod/api/group/router/api",
-  TASK: "https://7be2yaj3m1.execute-api.us-east-2.amazonaws.com/prod/api/task/router/api",
+  USER: "https://d3upgv9mgnyru0.cloudfront.net/api/user/router/api",
+  GROUP: "https://d3upgv9mgnyru0.cloudfront.net/api/group/router/api",
+  TASK: "https://d3upgv9mgnyru0.cloudfront.net/api/task/router/api"
 };
