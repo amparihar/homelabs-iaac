911fce0ad9c7542b7ba691e5dba1a5115b110f33
EC2 Default User <ec2-user@ip-172-31-20-97.us-east-2.compute.internal>
2022-01-24 13:09:50 +0000

    updated stateful overlay for meshed-todos-api

--- terraform-infra/eks-addons/modules/irsa/main.tf
+++ terraform-infra/eks-addons/modules/irsa/main.tf
@@ -1,51 +1,56 @@
 
 locals {
   assume_role_policy = data.aws_iam_policy_document.k8s_sa_assume_role_iam_policy.json
-  sa_role_enabled    = (var.appmesh_controller_enabled || var.kubernetes_external_secrets_enabled) ? 1 : 0
+  #sa_role_enabled    = (var.appmesh_controller_enabled || var.kubernetes_external_secrets_enabled) ? 1 : 0
 }
 
 # Appmesh controller role & policies
 resource "aws_iam_role" "sa" {
-  count              = local.sa_role_enabled
+  #count              = local.sa_role_enabled
   assume_role_policy = local.assume_role_policy
 }
 
 # add AWS managed IAM policies to appmesh controller sa
 resource "aws_iam_role_policy_attachment" "AWSAppMeshFullAccess" {
   count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "AWSCloudMapFullAccess" {
   count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudMapFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "AWSCloudMapDiscoverInstanceAccess" {
   count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCloudMapDiscoverInstanceAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "AWSAppMeshEnvoyAccess" {
   count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "AWSXRayDaemonWriteAccess" {
   count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "CloudWatchLogsFullAccess" {
-  count      = var.appmesh_controller_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+# Cloudwatch agent
+# resource "aws_iam_role_policy_attachment" "CloudWatchLogsFullAccess" {
+#   role       = aws_iam_role.sa.name
+#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+# }
 
 # kubernetes external secrets role & policies
 resource "aws_iam_policy" "kubernetes_external_secrets_sa" {
@@ -56,7 +61,7 @@ resource "aws_iam_policy" "kubernetes_external_secrets_sa" {
 
 resource "aws_iam_role_policy_attachment" "kubernetes_external_secrets_sa" {
   count      = var.kubernetes_external_secrets_enabled ? 1 : 0
-  role       = aws_iam_role.sa[count.index].name
+  role       = aws_iam_role.sa.name
   policy_arn = aws_iam_policy.kubernetes_external_secrets_sa[count.index].arn
 }
 
@@ -65,5 +70,5 @@ output "assume_role_policy" {
 }
 
 output "service_account_role_arn" {
-  value = length(aws_iam_role.sa.*.arn) > 0 ? aws_iam_role.sa[0].arn : "Please use the fargate pod execution role arn"
+  value = aws_iam_role.sa.arn 
 }
